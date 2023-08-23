77cb61924ff131ac4432950f629c9cddaa92b187
mbracconi <mauro.bracconi@polimi.it>
2023-08-23 12:10:48 +0200

    Updated compilation

--- libraries/odeChemistry/make.sh
+++ libraries/odeChemistry/make.sh
@@ -5,7 +5,7 @@ mkdir lib
 export BOOST_LIBS='-Wl,--start-group -Wl,-Bstatic -lboost_program_options -lboost_system -lboost_filesystem -lboost_regex -lboost_date_time -Wl,--end-group -Wl,-Bdynamic'
 
 echo "Compiling OdeChemistryNoMKL..."
-g++ -w -g -Iinclude -I../OpenSMOKEpp4catalyticFoam/include -I$EIGEN_LIBRARY_PATH -I$BOOST_LIBRARY_PATH/include/ -I$RAPIDXML_LIBRARY_PATH -L$BOOST_LIBRARY_PATH/lib/ -lgfortran -Wl,--no-as-needed -ldl -lpthread -lm $BOOST_LIBS $MKL_SUPPORT -fpic -c include/BatchReactorHeterogeneousConstantPressure.C include/BatchReactorHeterogeneousConstantVolume.C include/BatchReactorHomogeneousConstantPressure.C include/BatchReactorHomogeneousConstantVolume.C
+g++ -w -g -Iinclude -I../OpenSMOKEpp4catalyticFoam/include -I$EIGEN_LIBRARY_PATH -I$BOOST_LIBRARY_PATH/include/ -I$RAPIDXML_LIBRARY_PATH -L$BOOST_LIBRARY_PATH/lib/ -Wl,--no-as-needed -ldl -lpthread -lm $BOOST_LIBS $MKL_SUPPORT -fpic -c include/BatchReactorHeterogeneousConstantPressure.C include/BatchReactorHeterogeneousConstantVolume.C include/BatchReactorHomogeneousConstantPressure.C include/BatchReactorHomogeneousConstantVolume.C
 
 
 g++ -shared BatchReactorHeterogeneousConstantPressure.o BatchReactorHeterogeneousConstantVolume.o BatchReactorHomogeneousConstantPressure.o BatchReactorHomogeneousConstantVolume.o -o lib/libOdeChemistry.so 
